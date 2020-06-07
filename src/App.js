7fb4e49113dfa64d26cc2e62d191d3c87e38edc2
suMan <smn531@gmail.com>
2020-06-07 15:37:07 +0800

    New

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let person = {
+    name: "suMan",
+    job: "SHSEO"
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
         <p>My first react paragraph</p>
+        <h2>{person.name + " "+ person.job }</h2>
       
       </header>
     </div>
